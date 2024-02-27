093a6f088700a746b731edc04dfe31d851d1c47b
Zetaii <zerga556@yahoo.com>
2024-02-27 16:43:19 -0600

    Added Searchbar with working API
    SearchBar working, but attempting to figure out why click on outside not working

--- src/app/watchlist/page.tsx
+++ src/app/watchlist/page.tsx
@@ -1,40 +1,8 @@
-"use client"
+import React from "react"
 
-import MaxWidthWrapper from "@/src/components/MaxWidthWrapper"
-import React, { useState } from "react"
-import SearchBar from "@/src/components/SearchBar" // Adjust the path as needed
-
-interface Anime {
-  id: number
-  title: string
-  // Add more properties if needed
-}
-
-const Page: React.FC = () => {
-  const [searchResults, setSearchResults] = useState<Anime[]>([]) // Explicitly define the type of searchResults as Anime[]
-
-  const handleSearch = (results: Anime[]) => {
-    // Explicitly define the type of results parameter as Anime[]
-    setSearchResults(results)
-  }
-
-  return (
-    <>
-      <MaxWidthWrapper>
         <SearchBar onSearch={handleSearch} />
-        {/* Display search results or any other content */}
-        <ul>
-          {searchResults.map(
-            (
-              anime: Anime // Explicitly define the type of anime as Anime
-            ) => (
-              <li key={anime.id}>{anime.title}</li>
-            )
-          )}
-        </ul>
-      </MaxWidthWrapper>
-    </>
-  )
+const page = () => {
+  return <div></div>
 }
 
-export default Page
+export default page
